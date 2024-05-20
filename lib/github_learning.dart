6b0e236ee4efa6836a9150723d9c78c9635fbb2e
Faisal Shahzad <94742098+FaisalShahzad313@users.noreply.github.com>
2024-05-20 06:59:17 +0500

    creating for learning github and how it works

---
+++ lib/github_learning.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GitHubLearning extends StatelessWidget {
+  const GitHubLearning({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Github learning'),
+      ),
+    );
+  }
+}
